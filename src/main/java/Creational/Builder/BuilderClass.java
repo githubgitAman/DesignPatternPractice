d0c596a5b690604300e832bd4d12fdeb992ae5cb
githubgitAman <sharma.aman.a30@gmail.com>
2026-07-08 21:15:14 +0530

    Implemeted best approach of Builder

--- src/main/java/Creational/Builder/BuilderClass.java
+++ src/main/java/Creational/Builder/BuilderClass.java
@@ -38,4 +38,41 @@ public class BuilderClass {
     public void setAge(int age) {
         this.age = age;
     }
+    //Better Implementation of Builder
+    public static class BuilderClassHelper {
+        private String firstName;
+        private String lastName;
+        private int age;
+
+        public BuilderClass build() {
+            return new BuilderClass(this);
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public BuilderClassHelper setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public BuilderClassHelper setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public BuilderClassHelper setAge(int age) {
+            this.age = age;
+            return this;
+        }
+    }
 }
--- src/main/java/Creational/Builder/Client.java
+++ src/main/java/Creational/Builder/Client.java
@@ -2,13 +2,12 @@ package Creational.Builder;
 
 public class Client {
     public static void main(String[] args) {
-        //Creating abstraction by not directly creating Builder class object
-        BuilderClassHelper helperInstance = BuilderClass.getBuilder();
-        helperInstance.setFirstName("John");
-        helperInstance.setLastName("Doe");
-        helperInstance.setAge(24);
-        //Calling build method of Helper class
-        BuilderClass instance = helperInstance.build();
+        BuilderClass instance = BuilderClass.getBuilder()
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setAge(25)
+                .build();
+
         System.out.println(instance.getFirstName());
         System.out.println(instance.getLastName());
         System.out.println(instance.getAge());
